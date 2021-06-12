4b7a95a3ee699f0984517ac52e5b6f718a3e0750
Rishi <rishichaturvedi0012@gmail.com>
2021-06-12 19:11:58 +0530

    handling static files and Heroku configuration

--- django_task/django_task/settings.py
+++ django_task/django_task/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangotask-0.herokuapp.com', '127.0.0.1']
 
 AUTH_USER_MODEL = 'authentication.MyUser'
 
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
